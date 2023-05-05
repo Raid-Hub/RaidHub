92f05e2c64dc932d03b62f5ba4f5259efcc35ea9
Tim <tim2kaspar@gmail.com>
2023-05-05 11:46:34 +0200

    version, contact twitter socialTag and devs

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,22 @@
 import React from "react"
+import styles from "../styles/footer.module.css"
+import SocialTag from "./profile/SocialTag"
+import { Socials } from "../util/socials"
+
 
 type FooterProps = {}
 
+const developers = ["Theos", "Newo", "Bruce"]
+
 const Footer = ({}: FooterProps) => {
-    return <footer>Footer</footer>
+    return <footer className={styles["footer"]}>
+        <span className={styles["version"]}> v2023.1.3</span>
+        <div className={styles["contact"]}>
+            <span className={styles["contact-text"]}>Contact us</span>
+            <SocialTag id={Socials.Twitter} displayName={"@raidhub"} url={"https://www.twitter.com/raidhub"}/>
+        </div>
+        <span className={styles["developers"]}>Developed by {developers.join(", ")}</span>
+    </footer>
 }
 
 export default Footer
