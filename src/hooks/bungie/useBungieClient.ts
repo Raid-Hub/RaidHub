0f5b1a2e50a1b179a9bf3bfc26b9ef98768ee5dc
owensimpson <owensimpson1127@gmail.com>
2023-06-27 18:51:53 -0400

    test4

--- src/hooks/bungie/useBungieClient.ts
+++ src/hooks/bungie/useBungieClient.ts
@@ -6,6 +6,6 @@ export function useBungieClient(): BungieNetClient {
     const [client] = useState(new BungieNetClient())
     const { data } = useSession()
 
-    console.log(data)
+    console.log(data, client)
     return data?.client ?? client
 }
