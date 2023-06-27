3ee8857d65e251a0b797ce217eff0a405c7c1b5a
owensimpson <owensimpson1127@gmail.com>
2023-06-27 18:49:33 -0400

    test2

--- src/hooks/bungie/useBungieClient.ts
+++ src/hooks/bungie/useBungieClient.ts
@@ -1,16 +1,10 @@
 import { useSession } from "next-auth/react"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import BungieNetClient from "../../util/bungieClient"
 
 export function useBungieClient(): BungieNetClient {
-    const [client, setClient] = useState(new BungieNetClient())
+    const [client] = useState(new BungieNetClient())
     const { data } = useSession()
 
-    // useEffect(() => {
-    //     if (data?.client) {
-    //         setClient(data.client)
-    //     }
-    // }, [data?.client])
-
-    return client
+    return data?.client ?? client
 }
