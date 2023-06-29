4191c8fce145d1d652620d044f798062e74bd261
owensimpson <owensimpson1127@gmail.com>
2023-06-29 09:46:13 -0400

    update

--- next.config.js
+++ next.config.js
@@ -1,17 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  images: {
-    domains: ["lh3.googleusercontent.com"],
-  },
-  compiler: {
-    styledComponents: true,
-  },
-  env: {
-    BUNGIE_API_KEY: process.env.BUNGIE_API_KEY,
-    BUNGIE_CLIENT_ID: process.env.BUNGIE_CLIENT_ID,
-    BUNGIE_CLIENT_SECRET: process.env.BUNGIE_CLIENT_SECRET,
-  },
-};
+    reactStrictMode: false,
+    env: {
+        BUNGIE_API_KEY: process.env.BUNGIE_API_KEY,
+        BUNGIE_CLIENT_ID: process.env.BUNGIE_CLIENT_ID,
+        BUNGIE_CLIENT_SECRET: process.env.BUNGIE_CLIENT_SECRET
+    }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
