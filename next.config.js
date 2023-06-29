1821fd07e3bca6a1bf2aa85a9f62ed5e997ee82e
owensimpson <owensimpson1127@gmail.com>
2023-06-29 14:15:29 -0400

    added proxy server for auth

--- next.config.js
+++ next.config.js
@@ -2,9 +2,7 @@
 const nextConfig = {
     reactStrictMode: false,
     env: {
-        BUNGIE_API_KEY: process.env.BUNGIE_API_KEY,
-        BUNGIE_CLIENT_ID: process.env.BUNGIE_CLIENT_ID,
-        BUNGIE_CLIENT_SECRET: process.env.BUNGIE_CLIENT_SECRET
+        BUNGIE_API_KEY: process.env.BUNGIE_API_KEY
     }
 }
 
