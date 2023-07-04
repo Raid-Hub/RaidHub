2255e66155ad9779f937a10002144b7dc63ab68d
owensimpson <owensimpson1127@gmail.com>
2023-07-04 11:17:43 -0400

    fix missing stats

--- src/util/destiny/raidStatsMap.ts
+++ src/util/destiny/raidStatsMap.ts
@@ -2,17 +2,18 @@ import {
     DestinyAggregateActivityStats,
     DestinyHistoricalStatsValue
 } from "bungie-net-core/lib/models"
-import { AllValidHashes, HashDictionary, Raid, RaidDifficultyTuple, ValidRaidHash } from "./raid"
+import { AllValidHashes, Difficulty, HashDictionary, Raid, ValidRaidHash } from "./raid"
 import RaidStats from "../../models/profile/RaidStats"
 
 export type DestinyHistoricalStatsDictionary = { [key: string]: DestinyHistoricalStatsValue }
 
 export function raidStatsMap(stats: DestinyAggregateActivityStats[]): Map<Raid, RaidStats> {
-    const map = new Map<RaidDifficultyTuple, DestinyHistoricalStatsDictionary[]>()
+    // the key for this map is "Raid+Difficulty"
+    const map = new Map<string, DestinyHistoricalStatsDictionary[]>()
     stats.forEach(({ activityHash, values }) => {
         const hash = activityHash.toString() as ValidRaidHash
         if (AllValidHashes.includes(hash)) {
-            const key = HashDictionary[hash]
+            const key = HashDictionary[hash].join("+")
             if (map.has(key)) {
                 map.get(key)!.push(values)
             } else {
@@ -22,7 +23,9 @@ export function raidStatsMap(stats: DestinyAggregateActivityStats[]): Map<Raid, 
     })
 
     const raidMap = new Map<Raid, RaidStats>()
-    map.forEach((values, [raid, difficulty]) => {
+    map.forEach((values, key) => {
+        // deconstruct the key
+        const [raid, difficulty] = key.split("+").map(char => parseInt(char)) as [Raid, Difficulty]
         if (raidMap.has(raid)) {
             raidMap.get(raid)!.add(difficulty, values)
         } else {
