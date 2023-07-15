500a2010b08e22fe206f8dd2791189b0e026f0a9
Owen Simpson <owensimpson1127@gmail.com>
2023-07-15 00:58:15 -0400

    tag and tooltip fixes

--- src/components/profile/raids/RaidTagLabel.tsx
+++ src/components/profile/raids/RaidTagLabel.tsx
@@ -30,6 +30,7 @@ type RaidTagLabelProps = {
       } & RaceTag)
 )
 const RaidTagLabel = (props: RaidTagLabelProps) => {
+    const { strings } = useLocale()
     const [timer, setTimer] = useState<NodeJS.Timeout | null>(null)
 
     const scroll = () => {
@@ -45,36 +46,37 @@ const RaidTagLabel = (props: RaidTagLabelProps) => {
         timer && clearTimeout(timer)
     }, [timer])
 
-    const className = styles["clickable-tag"]
-    return props.instanceId ? (
-        <Link
-            className={className}
-            href={`/pgcr/${props.instanceId}`}
-            onMouseEnter={handleHover}
-            onMouseLeave={handleLeave}>
-            <InnerTag {...props} />
-        </Link>
-    ) : (
-        <div className={className}>
-            <InnerTag {...props} />
-        </div>
-    )
-}
-
-const InnerTag = (props: RaidTagLabelProps) => {
-    const { strings } = useLocale()
     const label =
         props.type === "challenge"
             ? getChallengeLabel(props, strings)
             : getRaceLabel(props, strings)
-    return (
-        <>
-            {props.type === "challenge" && props.bestPossible && (
-                <Image src={Diamond2} alt="mastery diamond" />
-            )}
-            {label && <span>{label}</span>}
-        </>
-    )
+
+    function InnerTag() {
+        return (
+            <>
+                {props.type === "challenge" && props.bestPossible && (
+                    <Image src={Diamond2} alt="mastery diamond" />
+                )}
+                <span>{label}</span>
+            </>
+        )
+    }
+
+    return label ? (
+        props.instanceId ? (
+            <Link
+                className={styles["clickable-tag"]}
+                href={`/pgcr/${props.instanceId}`}
+                onMouseEnter={handleHover}
+                onMouseLeave={handleLeave}>
+                <InnerTag />
+            </Link>
+        ) : (
+            <div className={styles["clickable-tag"]}>
+                <InnerTag />
+            </div>
+        )
+    ) : null
 }
 
 function getRaceLabel(props: RaceTag, strings: LocalStrings): string | null {
@@ -88,7 +90,24 @@ function getRaceLabel(props: RaceTag, strings: LocalStrings): string | null {
     }
 }
 
-function getChallengeLabel(tag: RaidTag, strings: LocalStrings): string {
+function getChallengeLabel(tag: RaidTag, strings: LocalStrings): string | null {
+    // special cases
+    let wishWall = false
+    if (tag.raid === Raid.DEEP_STONE_CRYPT && tag.playerCount === 1) {
+        return null
+    } else if (tag.raid === Raid.GARDEN_OF_SALVATION && tag.playerCount === 2) {
+        tag = {
+            ...tag,
+            fresh: false
+        }
+    } else if (tag.raid === Raid.VAULT_OF_GLASS && tag.playerCount === 1) {
+        tag = {
+            ...tag,
+            fresh: false
+        }
+    } else if (tag.raid === Raid.LAST_WISH && tag.playerCount === 1 && tag.fresh) {
+        wishWall = true
+    }
     const descriptors: string[] = []
     if (tag.fresh && !tag.flawless) descriptors.push(strings.tags[Tag.FRESH])
     switch (tag.playerCount) {
@@ -107,6 +126,7 @@ function getChallengeLabel(tag: RaidTag, strings: LocalStrings): string {
     else if (tag.difficulty == Difficulty.CONTEST) descriptors.push(strings.tags[Tag.CONTEST])
     if (tag.fresh == false) descriptors.push(strings.checkpoints[tag.raid])
     let str = descriptors.join(" ")
+    if (wishWall) str += " (Wishwall)"
     if (tag.fresh == null) str += "*"
     return str
 }
