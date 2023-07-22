af66c525c8567d16cd54b800d1ca670fc4d0b8dd
owensimpson <owensimpson1127@gmail.com>
2023-07-22 00:04:58 -0400

    test3

--- vercel/build.sh
+++ vercel/build.sh
@@ -9,32 +9,29 @@ if [[ -n $NAMESPACE ]] ; then
 
 elif [[ -n $STAGING ]] ; then
     echo "Deploying to staging..."
-    # Set the name of the branches
-    organization="raidhub"
-    database="app"
-    production_branch="main"
-    develop_branch="staging"
+    # TODO: flow to pull main db to staging db
+    # organization="raidhub"
+    # database="app"
+    # production_branch="main"
+    # develop_branch="staging"
+    # curl --request DELETE \
+    #     --url https://api.planetscale.com/v1/organizations/$organization/databases/$database/branches/$develop_branch \
+    #     --header "Authorization: $PLANETSCALE_TOKEN"
 
-    # Delete the develop branch
-    curl --request DELETE \
-        --url https://api.planetscale.com/v1/organizations/$organization/databases/$database/branches/$develop_branch \
-        --header "Authorization: $PLANETSCALE_TOKEN"
-
-    # create the new branch
-    curl --request POST \
-        --url https://api.planetscale.com/v1/organizations/$organization/databases/$database/branches \
-        --header "Authorization: $PLANETSCALE_TOKEN" \
-        --header 'accept: application/json' \
-        --header 'content-type: application/json' \
-        --data "
-    {
-        "name": "$develop_branch",
-        "parent_branch": "$production_branch"
-    }
-    "
+    # # create the new branch
+    # curl --request POST \
+    #     --url https://api.planetscale.com/v1/organizations/$organization/databases/$database/branches \
+    #     --header "Authorization: $PLANETSCALE_TOKEN" \
+    #     --header 'accept: application/json' \
+    #     --header 'content-type: application/json' \
+    #     --data "
+    # {
+    #     "name": "$develop_branch",
+    #     "parent_branch": "$production_branch"
+    # }
+    # "
 
     # apply the new prisma schema
-    yarn prisma generate
     yarn db:update
 
     yarn next build
