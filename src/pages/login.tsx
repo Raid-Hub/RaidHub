e3281c77cefa1a4b4538e1196093624ab2e4cfda
Owen Simpson <98496129+owensimpson@users.noreply.github.com>
2023-07-24 09:51:05 -0400

    Vanity optimization (#63)

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,16 +2,27 @@ import { NextPage } from "next"
 import { signIn } from "next-auth/react"
 import { useLocale } from "../components/app/LanguageProvider"
 import { useProviders } from "../hooks/app/useProviders"
+import { useRouter } from "next/router"
 
 const Login: NextPage = () => {
+    const { providers } = useProviders()
+    const { query } = useRouter()
     const { strings } = useLocale()
 
-    const { providers } = useProviders()
+    const callbackUrl = query["callbackUrl"]
     return (
         <main>
             <div style={{ display: "flex", gap: "1em" }}>
                 {providers?.map((provider, id) => (
-                    <button key={id} onClick={() => signIn(id)}>
+                    <button
+                        key={id}
+                        onClick={() =>
+                            signIn(id, {
+                                callbackUrl: Array.isArray(callbackUrl)
+                                    ? callbackUrl[0]
+                                    : callbackUrl
+                            })
+                        }>
                         {strings.logInWith + " " + provider.name}
                     </button>
                 ))}
