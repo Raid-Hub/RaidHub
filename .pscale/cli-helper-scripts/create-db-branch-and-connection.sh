a404a99e0b3bbad04c378c8ae914ea864d916d1e
Owen <98496129+owensimpson@users.noreply.github.com>
2023-07-28 18:24:23 -0400

    Planetscale CI/CD (#70)
    * planetscale-scripts

---
+++ .pscale/cli-helper-scripts/create-db-branch-and-connection.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+. use-pscale-docker-image.sh
+. wait-for-branch-readiness.sh
+
+. authenticate-ps.sh
+
+BRANCH_NAME="$1"
+
+. set-db-and-org-and-branch-name.sh
+
+. ps-create-helper-functions.sh
+if [[ $HAS_SCHEMA_CHANGES == true ]] ; then
+    create-db-branch "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME" "recreate"
+fi
+
+
+. create-branch-connection-string.sh
+create-branch-connection-string "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME"
+. ps-create-helper-functions.sh
+
+if [[ $HAS_SCHEMA_CHANGES == true ]] ; then
+    create-deploy-request "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME"
+fi
