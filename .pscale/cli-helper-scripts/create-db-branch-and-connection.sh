25699d7c4ac1e3371a1ddf426dceb06b528f4045
owensimpson <owensimpson1127@gmail.com>
2023-07-28 17:27:07 -0400

    test no schema changes2

--- .pscale/cli-helper-scripts/create-db-branch-and-connection.sh
+++ .pscale/cli-helper-scripts/create-db-branch-and-connection.sh
@@ -10,13 +10,15 @@ BRANCH_NAME="$1"
 . set-db-and-org-and-branch-name.sh
 
 . ps-create-helper-functions.sh
-create-db-branch "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME" "recreate"
+if [[ $HAS_SCHEMA_CHANGES == true ]] ; then
+    create-db-branch "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME" "recreate"
+fi
 
 
 . create-branch-connection-string.sh
 create-branch-connection-string "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME" "creds-${BRANCH_NAME}"
 . ps-create-helper-functions.sh
 
-if [[ $NEEDS_DR == true ]] ; then
+if [[ $HAS_SCHEMA_CHANGES == true ]] ; then
     create-deploy-request "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME"
 fi
