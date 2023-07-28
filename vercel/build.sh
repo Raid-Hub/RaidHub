e98944785067de89e40b3c201c2f9efbbe26360c
owensimpson <owensimpson1127@gmail.com>
2023-07-28 18:38:42 -0400

    update build text

--- vercel/build.sh
+++ vercel/build.sh
@@ -4,32 +4,37 @@ if [[ -n $NAMESPACE ]] ; then
 
   yarn prisma generate
   yarn prisma db push --accept-data-loss --skip-generate
-  yarn db:seed
+  yarn prisma db seed
+
   yarn next build
 
 elif [[ $APP_ENV == "staging" ]] ; then
-    echo "Deploying to staging..."
+  echo "Deploying to staging..."
 
-    yarn prisma generate
-    yarn db:update --skip-generate
-    yarn db:seed
+  yarn prisma generate
+  yarn prisma db push --skip-generate
+  yarn prisma db seed
 
-    yarn next build
+  yarn next build
 
 elif [[ $VERCEL_ENV == "production" ]] ; then 
   echo "Deploying to production..."
 
   yarn prisma generate
-  yarn db:update
+
   yarn next build
   
 elif [[ $VERCEL_ENV == "preview" ]] ; then 
   echo "Deploying to preview..."
 
-  yarn db:update
+  yarn prisma generate
+  yarn prisma db push --skip-generate
+  yarn prisma db seed
+
   yarn next build
 
 else
   echo "Building local..."
+  
   yarn next build
 fi
