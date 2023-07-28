20a163a416ee00573c1234ef92fc808317170a1e
Tim Kaspar <tim.kaspar@ti8m.ch>
2023-07-28 14:28:07 +0200

    add svgs and animations

--- src/components/pgcr/ActivityHeader.tsx
+++ src/components/pgcr/ActivityHeader.tsx
@@ -90,7 +90,127 @@ const ActivityHeader = ({ activity, pgcrLoadingState }: ActivityHeaderProps) => 
                         <p>{checkpointDisclaimer}</p>
                     </div>
                 )}
-                <button className={styles["screenshot-button"]} onClick={handleScreenshot}>Screenshot</button>
+                <motion.button
+                    className={styles["screenshot-button"]}
+                    onClick={handleScreenshot}
+                    data-html2canvas-ignore={true}
+                    animate={
+                        copied
+                            ? {
+                                  backgroundColor: [
+                                      "rgba(0,0,0,0)",
+                                      "rgba(0,0,0,0.4)",
+                                      "rgba(66, 245, 90, 0.7)"
+                                  ]
+                              }
+                            : {
+                                  backgroundColor: [
+                                      "rgba(66, 245, 90, 0.7)",
+                                      "rgba(0,0,0,0.4)",
+                                      "rgba(0,0,0,0)"
+                                  ]
+                              }
+                    }
+                    transition={{
+                        duration: 2
+                    }}>
+                    <motion.svg
+                        preserveAspectRatio={"xMidYMid"}
+                        viewBox={"0 0 63.19 63.19"}
+                        height={"20"}
+                        width={"20"}>
+                        <g>
+                            <motion.circle
+                                stroke={"#FFFFFF"}
+                                strokeWidth={"7"}
+                                fill={"transparent"}
+                                cx={31.59}
+                                cy={31.59}
+                                r={24}
+                                key={"circle"}
+                                initial={
+                                    copied ? { scale: 0, opacity: 0 } : { scale: 1, opacity: 1 }
+                                }
+                                animate={
+                                    copied ? { scale: 1, opacity: 1 } : { scale: 0, opacity: 0 }
+                                }
+                                transition={copied ? { delay: 1 } : { delay: 0 }}
+                            />
+                            <motion.path
+                                stroke={"#FFFFFF"}
+                                strokeWidth={"7"}
+                                strokeLinecap={"round"}
+                                fill={"transparent"}
+                                d={"M17.29 31.59l10.98 10.42 17.49-18.23"}
+                                key={"check"}
+                                initial={
+                                    copied
+                                        ? { opacity: 0, pathLength: 0 }
+                                        : { opacity: 1, pathLength: 1 }
+                                }
+                                animate={
+                                    copied
+                                        ? { opacity: 1, pathLength: 1 }
+                                        : { opacity: 0, pathLength: 0 }
+                                }
+                                transition={
+                                    copied
+                                        ? { delay: 1.2, duration: 1 }
+                                        : { delay: 0.2, duration: 1 }
+                                }
+                            />
+                        </g>
+                    </motion.svg>
+
+                    {/* TODO replace this with clipboard SVG */}
+                    <motion.svg
+                        preserveAspectRatio={"xMidYMid"}
+                        viewBox={"0 0 63.19 63.19"}
+                        height={"20"}
+                        width={"20"}>
+                        <g>
+                            <motion.circle
+                                stroke={"#FFFFFF"}
+                                strokeWidth={"7"}
+                                fill={"transparent"}
+                                cx={31.59}
+                                cy={31.59}
+                                r={24}
+                                key={"circle"}
+                                initial={
+                                    copied ? { scale: 1, opacity: 1 } : { scale: 0, opacity: 0 }
+                                }
+                                animate={
+                                    copied ? { scale: 0, opacity: 0 } : { scale: 1, opacity: 1 }
+                                }
+                                transition={copied ? { delay: 0 } : { delay: 1 }}
+                            />
+                            <motion.path
+                                stroke={"#FFFFFF"}
+                                strokeWidth={"7"}
+                                strokeLinecap={"round"}
+                                fill={"transparent"}
+                                d={"M17.29 31.59l10.98 10.42 17.49-18.23"}
+                                key={"check"}
+                                initial={
+                                    copied
+                                        ? { opacity: 1, pathLength: 1 }
+                                        : { opacity: 0, pathLength: 0 }
+                                }
+                                animate={
+                                    copied
+                                        ? { opacity: 0, pathLength: 0 }
+                                        : { opacity: 1, pathLength: 1 }
+                                }
+                                transition={
+                                    copied
+                                        ? { delay: 0.2, duration: 1 }
+                                        : { delay: 1.2, duration: 1 }
+                                }
+                            />
+                        </g>
+                    </motion.svg>
+                </motion.button>
             </div>
         </div>
     )
