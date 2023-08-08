288e864cc9753caa4937a924a801754b22fbbb87
owensimpson <owensimpson1127@gmail.com>
2023-08-08 16:39:06 -0400

    remove comment

--- src/components/profile/clan/ClanBanner.tsx
+++ src/components/profile/clan/ClanBanner.tsx
@@ -5,55 +5,49 @@ type ClanBannerProps = ClanBannerData
 
 const ClanBanner = ({ gonfalcons, decalTop, decalSecondary, decalPrimary }: ClanBannerProps) => {
     return (
-        <>
-            {/* <img src={`https://bungie.net${gonfalcons.path}`} />
-            <img src={`https://bungie.net${decalTop.path}`} /> */}
-            {/* <img src={`https://bungie.net${decalPrimary.path}`} />
-            <img src={`https://bungie.net${decalSecondary.path}`} /> */}
-            <svg
-                className={styles["clan-img"]}
-                xmlns="http://www.w3.org/2000/svg"
-                width="100%"
-                height="100%">
-                <defs>
-                    <mask id="gonfalcons">
-                        <image
-                            width="100%"
-                            height="100%"
-                            xlinkHref={`https://bungie.net${gonfalcons.path}`}
-                        />
-                    </mask>
-                    <mask id="topDecal">
-                        <image
-                            width="100%"
-                            height="100%"
-                            xlinkHref={`https://bungie.net${decalTop.path}`}
-                            mask="url(#gonfalcons)"
-                        />
-                    </mask>
-                    <mask id="decal2">
-                        <image
-                            width="100%"
-                            height="100%"
-                            xlinkHref={`https://bungie.net${decalSecondary.path}`}
-                            mask="url(#gonfalcons)"
-                        />
-                    </mask>
-                    <mask id="decal">
-                        <image
-                            width="100%"
-                            height="100%"
-                            xlinkHref={`https://bungie.net${decalPrimary.path}`}
-                            mask="url(#gonfalcons)"
-                        />
-                    </mask>
-                </defs>
-                <rect width="100%" height="100%" fill={gonfalcons.color} mask="url(#gonfalcons)" />
-                <rect width="100%" height="100%" fill={decalTop.color} mask="url(#topDecal)" />
-                <rect width="100%" height="100%" fill={decalSecondary.color} mask="url(#decal2)" />
-                <rect width="100%" height="100%" fill={decalPrimary.color} mask="url(#decal)" />
-            </svg>
-        </>
+        <svg
+            className={styles["clan-img"]}
+            xmlns="http://www.w3.org/2000/svg"
+            width="100%"
+            height="100%">
+            <defs>
+                <mask id="gonfalcons">
+                    <image
+                        width="100%"
+                        height="100%"
+                        xlinkHref={`https://bungie.net${gonfalcons.path}`}
+                    />
+                </mask>
+                <mask id="topDecal">
+                    <image
+                        width="100%"
+                        height="100%"
+                        xlinkHref={`https://bungie.net${decalTop.path}`}
+                        mask="url(#gonfalcons)"
+                    />
+                </mask>
+                <mask id="decal2">
+                    <image
+                        width="100%"
+                        height="100%"
+                        xlinkHref={`https://bungie.net${decalSecondary.path}`}
+                        mask="url(#gonfalcons)"
+                    />
+                </mask>
+                <mask id="decal">
+                    <image
+                        width="100%"
+                        height="100%"
+                        xlinkHref={`https://bungie.net${decalPrimary.path}`}
+                        mask="url(#gonfalcons)"
+                    />
+                </mask>
+            </defs>
+            <rect width="100%" height="100%" fill={gonfalcons.color} mask="url(#gonfalcons)" />
+            <rect width="100%" height="100%" fill={decalTop.color} mask="url(#topDecal)" />
+            <rect width="100%" height="100%" fill={decalSecondary.color} mask="url(#decal2)" />
+            <rect width="100%" height="100%" fill={decalPrimary.color} mask="url(#decal)" />
+        </svg>
     )
 }
 
