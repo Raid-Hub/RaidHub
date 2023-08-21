52e9c072619cdc7bbd70c4cb88b661ed554a0261
Owen Simpson <owensimpson1127@gmail.com>
2023-08-21 17:11:18 -0400

    update terms

--- src/pages/terms.tsx
+++ src/pages/terms.tsx
@@ -13,7 +13,7 @@ const TosPage: NextPage = () => {
                 <h4>
                     Effective date:{" "}
                     <span className={styles["effective-date"]}>
-                        {new Date("August 5, 2023 6:50:00 AM EDT").toLocaleDateString(undefined, {
+                        {new Date("August 21, 2023 5:06 AM EDT").toLocaleDateString(undefined, {
                             year: "numeric",
                             month: "long",
                             day: "numeric"
@@ -42,7 +42,79 @@ const TosPage: NextPage = () => {
                         contributes to the composition of our Site.
                     </p>
                     <h2>Accounts</h2>
-                    <p>TBD</p>
+                    <p>
+                        For login and account management, we use the service of Third-Party
+                        providers such as Bungie, Twitch, Discord and Twitter. All authentication is
+                        handled by the respective provider. Personal information is collected in an
+                        account on our servers and the respective providers can only access
+                        information required to perfrom the necessary functions of our site.
+                    </p>
+                    <p>When you create an account on our Site, you agree to the following:</p>
+                    <ol type="1">
+                        <p>
+                            <li>
+                                To provide true, accurate, current and complete information about
+                                yourself as prompted by the Company Service;
+                            </li>
+                        </p>
+                        <p>
+                            <li>
+                                that your account is for your personal and/or business use. You may
+                                not resell the Service;
+                            </li>
+                        </p>
+                        <p>
+                            <li>
+                                that by creating an account, you agree to receive certain
+                                communications in connection with the Service;
+                            </li>
+                        </p>
+                        <p>
+                            <li>
+                                as permitted, maintain and promptly update such information. If you
+                                provide any information that is false, inaccurate or outdated, or
+                                the Site has reasonable grounds to suspect that such information is
+                                false, inaccurate or outdated, the Site has the right to suspend or
+                                terminate your account and prohibit all current or future use of the
+                                Service by you; and
+                            </li>
+                        </p>
+                        <p>
+                            <li>
+                                as permitted, maintain and promptly update such information. If you
+                                provide any information that is false, inaccurate or outdated, the
+                                Site has reasonable grounds to suspect that such information is
+                                false, inaccurate or outdated, the Site has the right to suspend or
+                                terminate your account and prohibit all current or future use of the
+                                Service by you; and
+                            </li>
+                        </p>
+                        <p>
+                            <li>
+                                that you are responsible for maintaining the confidentiality of the
+                                password and account and are fully responsible for all activities
+                                that occur under your account. Your account is meant to be private
+                                and you shall not share accounts for any reason. You agree to
+                                immediately notify us of any unauthorized use of your password or
+                                account or any other breach of security. You agree to be responsible
+                                for all charges resulting from the use of your account via the
+                                Company Service, including charges resulting from unauthorized use
+                                of your account.
+                            </li>
+                        </p>
+                        <p>
+                            You may not impersonate someone else, create or use an account for
+                            anyone other than
+                        </p>
+                    </ol>
+                    <p>
+                        You may not impersonate someone else, create or use an account for anyone
+                        other than yourself, provide an email address other than your own, or create
+                        multiple accounts. If you use a pseudonym, take care to note that others may
+                        still be able to identify you if, for example, you include identifying
+                        information in your reviews, use the same account information on other
+                        sites, or allow other sites to share information about you with the Site.
+                    </p>
                     <h2>Links to Other Websites </h2>
                     <p>
                         Our Site contains links to third party websites or services that we do not
