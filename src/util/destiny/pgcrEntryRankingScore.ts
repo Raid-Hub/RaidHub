4dee65344280aff59dacd1123b971504bfa712ef
owensimpson <owensimpson1127@gmail.com>
2023-08-21 09:15:08 -0400

    improve ranking score

--- src/util/destiny/pgcrEntryRankingScore.ts
+++ src/util/destiny/pgcrEntryRankingScore.ts
@@ -15,12 +15,10 @@ export function pgcrEntryRankingScore({
     precisionKills: number
     superKills: number
 }) {
-    // kills weighted 2x assists
-    const killScore = (kills + 0.5 * assists) / Math.sqrt(round(timePlayedSeconds, -1))
-    // a multiplier based on your time per deaths squared, normalized a bit
+    // kills weighted 2x assists, slight diminishing returns
+    const killScore = (kills + 0.5 * assists) ** 0.95 / Math.sqrt(round(timePlayedSeconds, -1))
+    // a multiplier based on your time per deaths squared, normalized a bit by using deaths + 7
     const deathScore = (2 * timePlayedSeconds) / (deaths + 7) ** 2
-
-    const primaryScore = killScore * deathScore
 
     const timeScore = timePlayedSeconds / 360 // 10 points per hour
 
@@ -28,7 +26,16 @@ export function pgcrEntryRankingScore({
 
     const superScore = (superKills / (timePlayedSeconds / 60)) * 5 // 1 point per super kill per minute
 
-    const finalScore = primaryScore + timeScore + precisionScore + superScore
+    const finalScore = killScore * deathScore + timeScore + precisionScore + superScore
+
+    console.table({
+        killScore,
+        deathScore,
+        timeScore,
+        precisionScore,
+        superScore,
+        finalScore
+    })
 
     return finalScore
 }
