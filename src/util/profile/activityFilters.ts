91dc6247cdc2680cde60bcfb5f91f583fbd7a76f
owensimpson <owensimpson1127@gmail.com>
2023-10-07 15:39:09 -0400

    optimize default filter

--- src/util/profile/activityFilters.ts
+++ src/util/profile/activityFilters.ts
@@ -18,7 +18,8 @@ export enum FilterOption {
     CPB = "CP Bot",
     DIFFICULTY = "Difficulty",
     MIN_MINS_PLAYED = "Min Mins",
-    PLAYED_WITH = "Played With"
+    PLAYED_WITH = "Played With",
+    NONLOWMAN = "Not Lowman"
 }
 
 export const HighOrderActivityFilters = {
@@ -40,6 +41,7 @@ export const SingleActivityFilters = {
     [FilterOption.SUCCESS]: (activity: Activity) => !!activity.completed,
     [FilterOption.FLAWLESS]: (activity: Activity) => !!activity.flawless,
     [FilterOption.CPB]: (activity: Activity) => activity.playerCount > 50,
+    [FilterOption.NONLOWMAN]: (activity: Activity) => activity.playerCount > 3,
     [FilterOption.TRIO]: (activity: Activity) => activity.playerCount === 3,
     [FilterOption.DUO]: (activity: Activity) => activity.playerCount === 2,
     [FilterOption.SOLO]: (activity: Activity) => activity.playerCount === 1
@@ -56,6 +58,7 @@ export enum FilterListName {
     Solo,
     Duo,
     Trio,
+    NonLowman,
     Cpb,
     MinMinutes,
     Master,
@@ -76,6 +79,7 @@ export const FiltersToSelectFrom: Record<FilterListName, () => ActivityFilter> =
                 new SingleActivityFilter(FilterOption.TRIO)
             ])
         ]),
+    [FilterListName.NonLowman]: () => new SingleActivityFilter(FilterOption.NONLOWMAN),
     [FilterListName.Solo]: () => new SingleActivityFilter(FilterOption.SOLO),
     [FilterListName.Duo]: () => new SingleActivityFilter(FilterOption.DUO),
     [FilterListName.Trio]: () => new SingleActivityFilter(FilterOption.TRIO),
@@ -94,30 +98,24 @@ export const FiltersToSelectFrom: Record<FilterListName, () => ActivityFilter> =
 
 // min 15 mins played or lowman
 export const DefaultActivityFilters = new GroupActivityFilter("|", [
-    FiltersToSelectFrom[FilterListName.AnyLowman](),
+    FiltersToSelectFrom[FilterListName.Success](),
     new GroupActivityFilter("&", [
         FiltersToSelectFrom[FilterListName.MinMinutes](),
-        new NotActivityFilter(
-            new GroupActivityFilter("|", [
-                new SingleActivityFilter(FilterOption.SOLO),
-                new SingleActivityFilter(FilterOption.DUO),
-                new SingleActivityFilter(FilterOption.TRIO)
-            ])
-        ),
+        FiltersToSelectFrom[FilterListName.NonLowman](),
         new NotActivityFilter(FiltersToSelectFrom[FilterListName.Cpb]())
     ])
 ])
 
-export function decodeFilters(json: any): ActivityFilter | null {
+export function decodeFilters(json: unknown): ActivityFilter | null {
     if (!json) return null
     try {
         switch (typeof json) {
             case "object":
-                if (json["|"] && Array.isArray(json["|"])) {
+                if ("|" in json && Array.isArray(json["|"])) {
                     return new GroupActivityFilter("|", json["|"].map(decodeFilters))
-                } else if (json["&"] && Array.isArray(json["&"])) {
+                } else if ("&" in json && Array.isArray(json["&"])) {
                     return new GroupActivityFilter("&", json["&"].map(decodeFilters))
-                } else if (json["not"]) {
+                } else if ("not" in json) {
                     return new NotActivityFilter(decodeFilters(json["not"]))
                 } else if (Array.isArray(json)) {
                     const [key, value] = json
