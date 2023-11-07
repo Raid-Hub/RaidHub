b42be783bd83242adf7830783e4d109e234085fa
owensimpson <owensimpson1127@gmail.com>
2023-11-06 21:09:46 -0500

    fix diamond tags

--- src/util/raidhub/tags.ts
+++ src/util/raidhub/tags.ts
@@ -85,50 +85,72 @@ export function isBestTag(activity: Activity): boolean {
         case Raid.CROTAS_END:
             // solo crota or duo flawless or trio flawless master
             return (
-                activity.weight == 0b111000 ||
-                activity.weight == 0b011110 ||
-                activity.weight == 0b001111
+                bitfieldMatches(activity.weight, 0b111000) ||
+                bitfieldMatches(activity.weight, 0b011110) ||
+                bitfieldMatches(activity.weight, 0b001111)
             )
 
         case Raid.ROOT_OF_NIGHTMARES:
             // solo flawless or duo flawless master
-            return activity.weight == 0b111110 || activity.weight == 0b011111
+            return (
+                bitfieldMatches(activity.weight, 0b111110) ||
+                bitfieldMatches(activity.weight, 0b011111)
+            )
         case Raid.KINGS_FALL:
             // duo master oryx or trio flawless master
-            return activity.weight == 0b011001 || activity.weight == 0b001111
+            return (
+                bitfieldMatches(activity.weight, 0b011001) ||
+                bitfieldMatches(activity.weight, 0b001111)
+            )
         case Raid.VOW_OF_THE_DISCIPLE:
             // trio flawless master
-            return activity.weight == 0b001111
+            return bitfieldMatches(activity.weight, 0b001111)
         case Raid.VAULT_OF_GLASS:
-            // solo atheon or duo flawless master
-            return activity.weight == 0b111000 || activity.weight == 0b011111
+            // solo flawless vog or duo flawless master
+            return (
+                bitfieldMatches(activity.weight, 0b111110) ||
+                bitfieldMatches(activity.weight, 0b011111)
+            )
         case Raid.DEEP_STONE_CRYPT:
             // duo flawless
-            return activity.weight == 0b011110
+            return bitfieldMatches(activity.weight, 0b011110)
         case Raid.GARDEN_OF_SALVATION:
-            // duo sanc or trio flawless
-            return activity.weight == 0b011000 || activity.weight == 0b001110
+            // solo sanc or trio flawless
+            return (
+                bitfieldMatches(activity.weight, 0b111000) ||
+                bitfieldMatches(activity.weight, 0b001110)
+            )
         case Raid.CROWN_OF_SORROW:
             // duo flawless
-            return activity.weight == 0b011110
+            return bitfieldMatches(activity.weight, 0b011110)
         case Raid.SCOURGE_OF_THE_PAST:
             // duo insurrection or trio flawless
-            return activity.weight == 0b011000 || activity.weight == 0b001110
+            return (
+                bitfieldMatches(activity.weight, 0b011000) ||
+                bitfieldMatches(activity.weight, 0b001110)
+            )
         case Raid.LAST_WISH:
             // solo queens or trio flawless
-            return activity.weight == 0b111000 || activity.weight == 0b001110
+            return (
+                bitfieldMatches(activity.weight, 0b111000) ||
+                bitfieldMatches(activity.weight, 0b001110)
+            )
         case Raid.SPIRE_OF_STARS:
             // flawless prestige :(
-            return activity.weight == 0b000111
+            return bitfieldMatches(activity.weight, 0b000111)
         case Raid.EATER_OF_WORLDS:
             // solo prestige argos
-            return activity.weight == 0b111001
+            return bitfieldMatches(activity.weight, 0b111001)
         case Raid.LEVIATHAN:
             // duo prestige calus
-            return activity.weight == 0b011001
+            return bitfieldMatches(activity.weight, 0b011001)
         default:
             return false
     }
+}
+
+function bitfieldMatches(a: number, compareTo: number) {
+    return (a & compareTo) === compareTo
 }
 
 export function addModifiers(
