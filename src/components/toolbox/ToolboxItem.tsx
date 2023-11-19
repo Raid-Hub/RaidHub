0b1f83f5378231f94dd6a48e19b5a879f5654d2b
Tim <tim2kaspar@gmail.com>
2023-11-19 17:19:05 +0100

    move toolbox item to correct folder

--- src/components/toolbox/ToolboxItem.tsx
+++ src/components/toolbox/ToolboxItem.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import styles from "../../styles/toolbox.module.css"
+import styles from "~/styles/toolbox.module.css"
 
 const ToolboxItem = (props : {path : string, text : string}) => {
     return (
