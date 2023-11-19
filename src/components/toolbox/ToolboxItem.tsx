b4c5a763ac7a02f0d7bfa78e534fc9313d97c26c
Tim <tim2kaspar@gmail.com>
2023-11-19 22:17:43 +0100

    close toolbox on item select

--- src/components/toolbox/ToolboxItem.tsx
+++ src/components/toolbox/ToolboxItem.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 import styles from "~/styles/toolbox.module.css"
 
-const ToolboxItem = (props : {path : string, text : string}) => {
+const ToolboxItem = (props : {path : string, text : string, onClick: any}) => {
     return (
-        <Link href={props.path} className={styles["toolbox-item"]}>
+        <Link href={props.path} className={styles["toolbox-item"]} onClick={props.onClick}>
             {props.text}
         </Link>
     )
