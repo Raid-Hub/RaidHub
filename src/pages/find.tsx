247f7e9baf72c1cd1e416fbe5c912716bb1d3252
owensimpson <owensimpson1127@gmail.com>
2023-11-23 12:41:58 -0500

    require auth on find page

--- src/pages/find.tsx
+++ src/pages/find.tsx
@@ -1,14 +1,28 @@
 import { NextPage } from "next"
+import { signIn, useSession } from "next-auth/react"
 import Find from "~/components/find/find"
 import { useSearchParams } from "~/hooks/util/useSearchParams"
 import { activitySearchQuerySchema } from "~/services/raidhub/activitySearch"
 
 const FindPage: NextPage = () => {
+    const { status, data: sessionData } = useSession({
+        required: true,
+        onUnauthenticated() {
+            signIn("bungie", undefined, "reauth=false")
+        }
+    })
+
     const { isReady, query, replaceAll } = useSearchParams({
         decoder: q => activitySearchQuerySchema.parse(q)
     })
 
-    return isReady ? <Find query={query} replaceAllQueryParams={replaceAll} /> : null
+    return isReady && status === "authenticated" ? (
+        <Find
+            query={query}
+            replaceAllQueryParams={replaceAll}
+            sessionMembershipId={sessionData.user.destinyMembershipId}
+        />
+    ) : null
 }
 
 export default FindPage
