5501d0d7247cdc175f6a817e832b4bd9814a4902
owensimpson <owensimpson1127@gmail.com>
2023-11-25 18:31:25 -0500

    move to serverless drivers

--- src/server/prisma.ts
+++ src/server/prisma.ts
@@ -1,10 +1,20 @@
 // @ts-nocheck
+import { createRequire } from "node:module"
 import { PrismaClient } from "@prisma/client"
+import { PrismaPlanetScale } from "@prisma/adapter-planetscale"
+import { fetch as undiciFetch } from "undici"
 
 let prisma: PrismaClient
 
+function createPscaleClient() {
+    const require = createRequire(import.meta.url)
+    const { Client: PlanetScaleClient } = require("@planetscale/database")
+    const client = new PlanetScaleClient({ url: process.env.DATABASE_URL, fetch: undiciFetch })
+    const adapter = new PrismaPlanetScale(client)
+    return new PrismaClient({ adapter })
+}
 if (process.env.NODE_ENV === "production") {
-    prisma = new PrismaClient()
+    prisma = process.env.APP_ENV !== "local" ? createPscaleClient() : new PrismaClient()
 } else {
     if (!global.prisma) {
         global.prisma = new PrismaClient()
