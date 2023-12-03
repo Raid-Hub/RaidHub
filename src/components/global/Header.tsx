997baed6dddcf2a5417e46dc31b11e4518a1f423
Samuel Davis <41790962+NotBruce@users.noreply.github.com>
2023-12-03 19:30:54 +0000

    Move Dropdown to Component

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -1,29 +1,15 @@
 import Link from "next/link"
 import styles from "../../styles/header.module.css"
 import { useEffect, useState } from "react"
-import { signIn, signOut, useSession } from "next-auth/react"
-import { Variants, m } from "framer-motion"
-import { useLocale } from "../app/LocaleManager"
-import RightArrow from "~/images/icons/RightArrow"
 import SearchBar from "./SearchBar"
 import Image from "next/image"
 import Logo from "../../../public/logo.png"
 import AccountIcon from "./AccountIcon"
-
-const variants = {
-    open: {
-        height: "unset",
-        gridTemplateRows: "1fr"
-    },
-    closed: { height: "unset", gridTemplateRows: "0fr" }
-} satisfies Variants
+import AccountDropdown from "./AccountDropdown"
 
 const Header = () => {
-    const { data: sessionData } = useSession()
     const [isScrolled, setIsScrolled] = useState(false)
     const [isDropdownOpen, setIsDropdownOpen] = useState(false)
-
-    const { strings } = useLocale()
 
     useEffect(() => {
         const handleScroll = () => {
@@ -37,8 +23,6 @@ const Header = () => {
             window.removeEventListener("scroll", handleScroll)
         }
     }, [])
-
-    const animate: keyof typeof variants = isDropdownOpen ? "open" : "closed"
 
     return (
         <>
@@ -63,81 +47,7 @@ const Header = () => {
                     />
                 </div>
             </header>
-            {isDropdownOpen && (
-                <m.div
-                    className={styles["account-dropdown-content-container"]}
-                    initial={"closed"}
-                    animate={animate}
-                    variants={variants}>
-                    <div className={styles["account-dropdown-content"]}>
-                        {sessionData ? (
-                            <>
-                                <div className={styles["account-dropdown-top"]}>
-                                    <div className={styles["account-dropdown-top-user"]}>
-                                        <a className={styles["account-dropdown-name"]}>
-                                            {sessionData.user.name}
-                                        </a>
-                                        <a className={styles["account-dropdown-id"]}>
-                                            {sessionData.user.destinyMembershipId}
-                                        </a>
-                                    </div>
-                                </div>
-                                <hr style={{ borderColor: "var(--border)" }} />
-                                {sessionData.user.destinyMembershipType &&
-                                    sessionData.user.destinyMembershipId && (
-                                        <div className={`${styles["card-section"]}`}>
-                                            <Link
-                                                href={`/profile/${sessionData.user.destinyMembershipType}/${sessionData.user.destinyMembershipId}`}
-                                                className={styles["content-section"]}>
-                                                <div>
-                                                    <h4>{strings.viewProfile}</h4>
-                                                </div>
-                                                <div className={styles["content-section-arrow"]}>
-                                                    <RightArrow />
-                                                </div>
-                                            </Link>
-                                        </div>
-                                    )}
-                                <div className={`${styles["card-section"]}`}>
-                                    <Link href="/account" className={styles["content-section"]}>
-                                        <div>
-                                            <h4>{strings.manageAccount}</h4>
-                                        </div>
-                                        <div className={styles["content-section-arrow"]}>
-                                            <RightArrow />
-                                        </div>
-                                    </Link>
-                                </div>
-                                <div
-                                    onClick={() => signOut({ callbackUrl: "/" })}
-                                    className={styles["content-section"]}>
-                                    <div>
-                                        <span>{strings.logOut}</span>
-                                    </div>
-                                    <div className={styles["content-section-arrow"]}>
-                                        <RightArrow />
-                                    </div>
-                                </div>
-                            </>
-                        ) : (
-                            <div
-                                onClick={() => {
-                                    signIn("bungie", {
-                                        callbackUrl: encodeURI(window.location.href)
-                                    })
-                                }}
-                                className={styles["content-section"]}>
-                                <div>
-                                    <span>{strings.logIn}</span>
-                                </div>
-                                <div className={styles["content-section-arrow"]}>
-                                    <RightArrow />
-                                </div>
-                            </div>
-                        )}
-                    </div>
-                </m.div>
-            )}
+            <AccountDropdown isDropdownOpen={isDropdownOpen} />
         </>
     )
 }
