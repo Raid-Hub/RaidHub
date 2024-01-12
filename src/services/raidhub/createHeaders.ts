a082689850ca9a636493ba584dd1a4e24b252885
Owen Simpson <owensimpson1127@gmail.com>
2024-01-11 20:21:40 -0500

    fix server api key

--- src/services/raidhub/createHeaders.ts
+++ src/services/raidhub/createHeaders.ts
@@ -1,4 +1,4 @@
-export function createHeaders(server: boolean?): HeadersInit | undefined {
-    if (server) return { "x-api-key": process.env.RAIDHUB_API_KEY_SERVER }
+export function createHeaders(server?: boolean): HeadersInit | undefined {
+    if (server) return { "x-api-key": process.env.RAIDHUB_API_KEY_SERVER! }
     return process.env.RAIDHUB_API_KEY ? { "x-api-key": process.env.RAIDHUB_API_KEY } : undefined
 }
