1458965ca7dc1196a5ac140d6b773266f058bdb4
Owen <98496129+owensimpson@users.noreply.github.com>
2024-01-17 21:33:59 -0500

    Update temp worldfirst board ids

--- src/data/leaderboards.ts
+++ src/data/leaderboards.ts
@@ -3,54 +3,54 @@ import { ListedRaid, Raid } from "~/types/raids"
 export type Board = "normal" | "prestige" | "pc" | "challenge" | "master"
 export const LeaderboardsForRaid = {
     [Raid.LEVIATHAN]: {
-        normal: "wf_levi",
-        prestige: "levi_prestige",
+        normal: "leviathan_normal",
+        prestige: "leviathan_prestige",
         pc: "levi_pc"
     },
     [Raid.EATER_OF_WORLDS]: {
-        normal: "wf_eow",
-        prestige: "levi_prestige"
+        normal: "eaterofworlds_normal",
+        prestige: "eaterofworlds_prestige"
     },
     [Raid.SPIRE_OF_STARS]: {
-        normal: "wf_spire",
-        prestige: "spire_prestige"
+        normal: "spireofstars_normal",
+        prestige: "spireofstars_prestige"
     },
     [Raid.LAST_WISH]: {
-        normal: "wf_wish"
+        normal: "lastwish_normal"
     },
     [Raid.SCOURGE_OF_THE_PAST]: {
-        normal: "wf_sotp"
+        normal: "scourgeofthepast_normal"
     },
     [Raid.CROWN_OF_SORROW]: {
-        normal: "wf_cos"
+        normal: "crownofsorrow_normal"
     },
     [Raid.GARDEN_OF_SALVATION]: {
-        normal: "wf_gos"
+        normal: "gardenofsalvation_normal"
     },
     [Raid.DEEP_STONE_CRYPT]: {
-        normal: "wf_dsc"
+        normal: "deepstonecrypt_normal"
     },
     [Raid.VAULT_OF_GLASS]: {
-        normal: "vog_normal",
-        challenge: "wf_vog",
-        master: "vog_master"
+        normal: "vaultofglass_normal",
+        challenge: "vaultofglass_challenge",
+        master: "vaultofglass_master"
     },
     [Raid.VOW_OF_THE_DISCIPLE]: {
-        normal: "wf_vow",
-        master: "vow_master"
+        normal: "vowofthedisciple_normal",
+        master: "vowofthedisciple_master"
     },
     [Raid.KINGS_FALL]: {
-        normal: "kf_normal",
-        challenge: "wf_kf",
-        master: "kf_master"
+        normal: "kingsfall_normal",
+        challenge: "kingsfall_challenge",
+        master: "kingsfall_master"
     },
     [Raid.ROOT_OF_NIGHTMARES]: {
-        normal: "wf_ron",
-        master: "ron_master"
+        normal: "rootofnightmares_normal",
+        master: "rootofnightmares_master"
     },
     [Raid.CROTAS_END]: {
-        normal: "crota_normal",
-        challenge: "wf_crota",
-        master: "crota_master"
+        normal: "crotasend_normal",
+        challenge: "crotasend_challenge",
+        master: "crotasend_master"
     }
 } as const satisfies Record<ListedRaid, Partial<Record<Board, string>>>
