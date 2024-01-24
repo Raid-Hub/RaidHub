f76939927909024b16c2bfccef3bf749a2227dda
Owen Simpson <owensimpson1127@gmail.com>
2024-01-24 11:04:42 -0500

    update prisma declaration file

--- src/server/prisma.ts
+++ src/server/prisma.ts
@@ -1,23 +1,31 @@
-// @ts-nocheck
 import { PrismaClient } from "@prisma/client"
 import { Client as PlanetScaleClient } from "@planetscale/database"
-import { PrismaPlanetScale } from "@prisma/adapter-planetscale"
+import { PrismaPlanetScale as PrismaPlanetScaleAdapter } from "@prisma/adapter-planetscale"
 import { fetch as undiciFetch } from "undici"
+
+declare global {
+    var prisma: PrismaClient | undefined
+}
 
 let prisma: PrismaClient
 
-function createPscaleClient() {
-    const client = new PlanetScaleClient({ url: process.env.DATABASE_URL, fetch: undiciFetch })
-    const adapter = new PrismaPlanetScale(client)
-    return new PrismaClient({ adapter })
-}
 if (process.env.NODE_ENV === "production") {
-    prisma = process.env.APP_ENV !== "local" ? createPscaleClient() : new PrismaClient()
+    prisma = process.env.APP_ENV === "local" ? createLocalClient() : createPscaleClient()
 } else {
     if (!global.prisma) {
-        global.prisma = new PrismaClient()
+        global.prisma = createLocalClient()
     }
     prisma = global.prisma
 }
 
 export default prisma
+
+function createLocalClient() {
+    return new PrismaClient()
+}
+
+function createPscaleClient() {
+    const client = new PlanetScaleClient({ url: process.env.DATABASE_URL, fetch: undiciFetch })
+    const adapter = new PrismaPlanetScaleAdapter(client)
+    return new PrismaClient({ adapter })
+}
