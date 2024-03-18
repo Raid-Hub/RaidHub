723e2b17465192718c625c673e2202b08fb296fb
Owen Simpson <owensimpson1127@gmail.com>
2024-03-17 21:54:42 -0400

    revert and test

--- .github/scripts/turso-migrate.sh
+++ .github/scripts/turso-migrate.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 # Check if the schema matches the migration files
 yarn prisma migrate diff --from-migrations "./prisma/migrations" --to-schema-datamodel "./prisma/schema.prisma" --exit-code 1>/dev/null
 exit_code=$?
@@ -13,7 +14,7 @@ elif [ $exit_code -eq 1 ]; then
 fi
 
 # read applied schema from the database
-applied_migrations_list=$(/home/runner/.turso/turso db shell $TURSO_DATABASE_NAME "SELECT id FROM _migration WHERE is_applied = true")
+applied_migrations_list=$(turso db shell $TURSO_DATABASE_NAME "SELECT id FROM _migration WHERE is_applied = true")
 
 if [ $? -ne 0 ]; then
     echo "Failed to read from database"
@@ -23,12 +24,12 @@ fi
 for dir in ./prisma/migrations/*/; do
     migration=$(basename "$dir")
     if ! grep -q "$migration" <<< "$applied_migrations_list"; then
-        /home/runner/.turso/turso db shell $TURSO_DATABASE_NAME < $dir/migration.sql
+        turso db shell $TURSO_DATABASE_NAME < $dir/migration.sql
         if [ $? -ne 0 ]; then
           echo "Failed to apply migration $migration"
           exit 1
         else
-          /home/runner/.turso/turso db shell $TURSO_DATABASE_NAME "INSERT INTO _migration (id, is_applied) VALUES ('$migration', true) ON CONFLICT(id) DO UPDATE SET is_applied = excluded.is_applied;"
+          turso db shell $TURSO_DATABASE_NAME "INSERT INTO _migration (id, is_applied) VALUES ('$migration', true) ON CONFLICT(id) DO UPDATE SET is_applied = excluded.is_applied;"
           if [ $? -ne 0 ]; then
             echo "Failed to update database"
             exit 1
