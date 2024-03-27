50e973af1dd83c46b80e44c4c78dd2d2b7a1fa26
Owen Simpson <owensimpson1127@gmail.com>
2024-03-27 10:10:48 -0400

    minor UI fixes

--- src/components/TabSelector.tsx
+++ src/components/TabSelector.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import { $media } from "~/app/layout/media"
 import { Flex } from "./layout/Flex"
 
 export const TabSelector = styled(Flex)`
@@ -12,11 +13,19 @@ export const TabSelector = styled(Flex)`
     & > * {
         cursor: pointer;
     }
+
+    ${$media.max.mobile`
+        & h4 {
+            font-size: 0.75rem;
+        }
+    `}
 `
 
 TabSelector.defaultProps = {
+    $padding: 0.5,
     $gap: 0,
     $direction: "row",
     $align: "flex-start",
-    $crossAxis: "flex-end"
+    $crossAxis: "flex-end",
+    $wrap: false
 }
