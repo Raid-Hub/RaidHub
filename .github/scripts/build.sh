9201305d73bdd2f6646bf7128f1abadecd2a6d28
Owen Simpson <owensimpson1127@gmail.com>
2024-06-17 20:09:44 -0400

    check commit ref

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,9 +1,7 @@
-if [ "$APP_ENV" == "staging" ]; then
+if [ "$VERCEL_GIT_COMMIT_REF" = "test" ] && [ "$VERCEL_ENV" = "preview" ]; then
     # Staging
     export APP_VERSION=$(date '+%Y%m%d')-$VERCEL_GIT_COMMIT_SHA
     export DEPLOY_URL="https://staging.raidhub.io"
-
-    printenv
 
     bun prisma generate
     bun next build
