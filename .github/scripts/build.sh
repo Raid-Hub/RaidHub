5743dc81dbab10194924bb42f81c4162afb21ac9
Owen Simpson <owensimpson1127@gmail.com>
2024-06-17 20:19:46 -0400

    test preview

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,8 +1,19 @@
 if [ "$VERCEL_GIT_COMMIT_REF" = "test" ] && [ "$VERCEL_ENV" = "preview" ]; then
     # Staging
+    export APP_ENV="staging"
     export APP_VERSION=$(date '+%Y%m%d')-$VERCEL_GIT_COMMIT_SHA
     export DEPLOY_URL="https://staging.raidhub.io"
 
     bun prisma generate
     bun next build
+
+elif [ "$VERCEL_ENV" = "preview" ]; then
+    # Preview
+    export APP_VERSION=$(date '+%Y%m%d')-$VERCEL_GIT_COMMIT_SHA
+
+    bun prisma generate
+    bun next build
+
+elif [ "$VERCEL_ENV" = "production" ]; then
+    # Prod
 fi
