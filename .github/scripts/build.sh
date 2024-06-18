7cc2d49cb71e251f893562f4c1c7e5af1aed1fba
Owen Simpson <owensimpson1127@gmail.com>
2024-06-17 20:03:17 -0400

    app_env

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,8 +1,10 @@
-# Staging
-export APP_VERSION=$(date '+%Y%m%d')-$(git rev-parse --short HEAD)
-export DEPLOY_URL="https://staging.raidhub.io"
+if $APP_ENV == "staging"; then
+    # Staging
+    export APP_VERSION=$(date '+%Y%m%d')-$VERCEL_GIT_COMMIT_SHA
+    export DEPLOY_URL="https://staging.raidhub.io"
 
-printenv
+    printenv
 
-bun prisma generate
-bun next build
+    bun prisma generate
+    bun next build
+fi
